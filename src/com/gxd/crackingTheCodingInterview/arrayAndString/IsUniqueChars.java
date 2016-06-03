3707739477e54062f70b2492d5c2fdd20ca16530
gxd <xiaodaguan@gmail.com>
2016-06-03 18:07:09 +0800

    add: 置空矩阵中0元素所在的行和列

--- src/com/gxd/crackingTheCodingInterview/arrayAndString/IsUniqueChars.java
+++ src/com/gxd/crackingTheCodingInterview/arrayAndString/IsUniqueChars.java
@@ -4,33 +4,12 @@ import sun.text.normalizer.UnicodeSet;
 
 /**
  * Created by guanxiaoda on 16/4/19.
- * �ַ�������ĸ�Ƿ�ֻ����һ��(�Ƿ�����ظ����ֵ��ַ�)
- * ���:
- * �����ַ���&����:ascii, gb2312, big5, gb18030, unicode
- * ascii: 256��������ĸ\����������\Ӣ�ı��
- * gbXXXX: ����\��ѧ����\����ϣ����ĸ\���ļ���\ȫ���ַ�(gb2312->gbk/gb18030)
- * big5: ����������������
- * <p/>
- * unicode�ַ���: ͳһ�������ҵ������ַ���(ͳһ��\�����)
- * utf-8/utf-16/utf-32: ���ֱ��뷽��
- * <p/>
- * int���͵ķ�Χ:
- * c����ֻ�涨int���������ܱ�ʾ-32767~32767֮�������
- * ʵ�ʵķ�ΧҪ��ƽ̨�ͱ�����
- * 2^32 - 1 = 2147483647
- * - 2^32 = 2147483648
- * ����������ʾ32ά������,���ٶ�ֻ��26����ĸ��ʱ��(��ʵ���뺺��Ҳ����,why? '��'-'a'�Ĳ�ֵΪ2���),������һ��int���͵���������ʾ��ĸ����ÿ���ַ��Ƿ���ֳ���1��
- * ��0����ĸ->a
- * ��1����ĸ->b
- * ��n����ĸ->x-'a'
- * <p/>
- * ��λ��: &
+ *
  */
 public class IsUniqueChars {
 
     /**
-     * ����һ������,����Ϊ��ĸ����,ÿ��λ���ϵ�ֵΪ���ַ����ַ����г��ֵĴ���
-     * ֻҪ��һ��ֵ>1,���ַ�������uniquechars
+     *
      */
     public static boolean isUniqueChars1(String str) {
         if (str == null) return true;
@@ -46,7 +25,6 @@ public class IsUniqueChars {
     }
 
     /**
-     * ��һ������(int��ʾ��������)��ʾ����,��Ϊ����ÿ��λ����ֻ��Ҫ0����1�Ϳ���
      *
      * @param str
      * @return
@@ -95,7 +73,7 @@ public class IsUniqueChars {
 //        System.out.println(isUniqueChars2("abcdeff"));
 //        System.out.println(isUniqueChars2("zxcvfjk"));
 //        System.out.println(isUniqueChars2("����һ���й���"));
-        System.out.println(isUniqueChars2("�� ��һ��������"));
+        System.out.println(isUniqueChars2("我是一个中国人吗"));
 
     }
 }
