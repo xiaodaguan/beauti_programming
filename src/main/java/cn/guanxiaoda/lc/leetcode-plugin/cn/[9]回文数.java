65820d16ed267530fb93937a2cbc8ec4a06cb840
guanxiaoda <xiaodaguan@gmail.com>
2019-05-26 15:50:23 +0800

    hws

---
+++ src/main/java/cn/guanxiaoda/lc/leetcode-plugin/cn/[9]回文数.java
@@ -0,0 +1,51 @@
+//判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
+//
+// 示例 1: 
+//
+// 输入: 121
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: -121
+//输出: false
+//解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+// 
+//
+// 示例 3: 
+//
+// 输入: 10
+//输出: false
+//解释: 从右向左读, 为 01 。因此它不是一个回文数。
+// 
+//
+// 进阶: 
+//
+// 你能不将整数转为字符串来解决这个问题吗？ 
+//
+
+@SuppressWarnings("ALL")
+class Solutionhws {
+
+
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x == 0) {
+            return true;
+        }
+        if (x % 10 == 0) {
+            return false;
+        }
+        int re = 0;
+        while (x > re) {
+            int num = x % 10;
+            x = x / 10;
+            re = re * 10 + num;
+        }
+        return x == re || x == re / 10;
+    }
+}
